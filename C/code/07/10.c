8b5d2a5dade8e2d8761e894de9515763fd16eabf
pinstinct <gusals3407@gmail.com>
2020-10-01 17:14:49 +0900

    update

--- C/code/07/10.c
+++ C/code/07/10.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/* goto문을 제거하고 반복문을 이용해서 같은 결과를 얻을 수 있도록 프로그램을 변경합니다.
+int nInput;
+
+INPUT:
+    printf("Input number : ");
+    scanf("%d", &nInput);
+
+    if (nInput < 0 || nInput > 10)
+        goto INPUT;
+
+    puts("End");
+*/
 int main(void)
 {
     int nInput;
